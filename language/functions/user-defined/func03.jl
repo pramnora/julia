ec77f8492c6b8b39429124610cb8b8fc9070b5d5
Paul Ramnora <pramnora@yahoo.com>
2021-09-26 02:45:45 +0100

    Update func03.jl
    Minor edit: Added explanatory code comments

--- language/functions/user-defined/func03.jl
+++ language/functions/user-defined/func03.jl
@@ -1,5 +1,5 @@
-function add(a,b)
-  return a+b
+function add(a,b) # double argument list, with comma separated variables
+  return a+b      # returns a mathematical expression sum
 end
 
-println(add(1,3))
+println(add(1,3)) # function call
