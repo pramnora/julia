2ebf18e05845e9713a09f2e5df60e928c96bbb5a
Paul Ramnora <pramnora@yahoo.com>
2021-09-26 02:57:04 +0100

    Create func01.jl
    Created file: [func01.jl]/Prints out a simple 'Hello, world!' text message...using a function with an 'empty' argument list: ()

---
+++ language/functions/user-defined/func01.jl
@@ -0,0 +1,5 @@
+function printMsg()        # create function using an empty argument list: ()
+  println("Hello, world!") # send text message to output screen
+end
+
+printMsg()                  # function call
