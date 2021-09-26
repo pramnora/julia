7262394e75379cf0df0cb2b9bf74fd65fa95acd4
Paul Ramnora <pramnora@yahoo.com>
2021-09-26 02:32:50 +0100

    Create if-block-01.jl
    Created file: [if-block-01.jl]/Shows how to use an 'if block' statement, including: if-elseif-else-if

---
+++ language/selection/if/if-block-01.jl
@@ -0,0 +1,18 @@
+# *** Variable declaration...
+
+x = 1
+# x = 2
+# x = 3
+
+# *** If block: if-elseif-else-end
+
+if x == 1
+   println("x=1")
+elseif x == 2
+   println("x=2")
+else
+   println("x NOT 1/x NOT 2")
+end
+
+# Output...
+# x=1
