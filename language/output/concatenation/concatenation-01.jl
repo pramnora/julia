660aae82bfd9be8957368ae9dca5f379570be334
Paul Ramnora <pramnora@yahoo.com>
2024-08-12 00:31:57 +0100

    Create concatenation-01.jl
    Added file: [concatenation-01.jl] Shows 3 x different methods of joining together 2 strings; then, printing those 2 strings out.

---
+++ language/output/concatenation/concatenation-01.jl
@@ -0,0 +1,15 @@
+# Variable declarations...
+
+a="Hello, "
+b="world!"
+
+# String concatenation...
+
+print(a,b)
+# Hello, world!
+
+print(string(a,b))
+# Hello, world!
+
+print(a*b)
+# Hello, world!
