ce6cc84adb164ccd965bbf1075767d9a169c1488
Paul Ramnora <pramnora@yahoo.com>
2024-08-11 16:58:20 +0100

    Update while01.jl
    Demonstrating how to use the While loop to count going both forwards(1 up to 10)/backwards(10 down to 1)

--- language/loops/while/while01.jl
+++ language/loops/while/while01.jl
@@ -1,6 +1,17 @@
+# while loop counts going upwards from 1 to 10...
+
 x=1
 while x<10
   print(x," ")
   x+=1
 end
 #1 2 3 4 5 6 7 8 9 10
+
+# while loop counts going backwards from 10 down to 1...
+
+x=10
+while x>0
+  print(x," ")
+  x-=1
+end
+#10 9 8 7 6 5 4 3 2 1
