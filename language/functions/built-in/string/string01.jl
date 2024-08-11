8a3c359b9f90f6959556457f3a2b74280cb425e9
Paul Ramnora <pramnora@yahoo.com>
2024-08-11 19:20:30 +0100

    Create string01.jl
    Initial commit. Created file: [string01.jl] Shows how to use strings/with their build in functions.

---
+++ language/functions/built-in/string/string01.jl
@@ -0,0 +1,24 @@
+# --------------------------------------------------
+# declare string/and, print out it's value/length...
+# --------------------------------------------------
+
+hw="Hello, world!"
+print(hw)
+# Hello, world!
+
+print(length(hw))
+# 13
+
+# -----------------------------
+# string splicing techniques...
+# -----------------------------
+
+# print(hw[1])
+# h
+
+# print(hw[end])
+# !
+
+#print(hw[1:4])
+# Hell
+#* NOTE: String index lengths start at 1/not 0...as in many other programming languages
