ace4534dbb855fa11dd130a91a2c57c19dba79e7
Paul Ramnora <pramnora@yahoo.com>
2024-08-11 15:45:17 +0100

    Update for01.jl
    updated file with an 'println()' example...that, automatically, uses line breaks at the end of each 'print'

--- language/loops/for/for01.jl
+++ language/loops/for/for01.jl
@@ -22,11 +22,25 @@ end
 # 9
 # 10
 
+for x = 1:10
+  println(x)
+end
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+
 # ---------------------------------------------------------------------------------
 
-# NOTE: It order to try the above code...; 
-#       you do NOT need to type it in starting from scratch every single time;
-#       instead, just use your up key arrow...; until you find the line you want...;
-#       and, then, use your horizontal arrow key to change/edit the line of code;
-#       then, when done use the down arrow key...to go past the 'end' statement;
-#       finally, just press the [ENTER] key to run.
+# NOTE(1): It order to try the above code...; 
+#          you do NOT need to type it in starting from scratch every single time;
+#          instead, just use your up key arrow...; until you find the line you want...;
+#          and, then, use your horizontal arrow key to change/edit the line of code;
+#          then, when done use the down arrow key...to go past the 'end' statement;
+#          finally, just press the [ENTER] key to run.
