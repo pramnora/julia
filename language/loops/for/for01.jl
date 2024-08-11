559600cf2738d9f6576d7a394151fbaa6deba447
Paul Ramnora <pramnora@yahoo.com>
2024-08-11 15:28:48 +0100

    Create for01.jl
    Initial commit. Created file: [for01.jl] Demonstrates 3 different ways of using the Julia for loop...to print out digits: 1 up to 10...; each time producing a slightly different printout effect.

---
+++ language/loops/for/for01.jl
@@ -0,0 +1,32 @@
+for x = 1:10
+  print(x)
+end
+# 12345678910
+
+for x = 1:10
+  print(x," ")
+end
+# 1 2 3 4 5 6 7 8 9 10
+
+for x = 1:10
+  print(x,"\n")
+end
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+
+# ---------------------------------------------------------------------------------
+
+# NOTE: It order to try the above code...; 
+#       you do NOT need to type it in starting from scratch every single time;
+#       instead, just use your up key arrow...; until you find the line you want...;
+#       and, then, use your horizontal arrow key to change/edit the line of code;
+#       then, when done use the down arrow key...to got past the 'end' statement;
+#       finally, just press the [ENTER] key to run.
